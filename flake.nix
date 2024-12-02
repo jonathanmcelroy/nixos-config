4a8463e174d4a7a5277eb8a48d47f92af1f043e8
Jonathan McElroy <jonathanpmcelroy@gmail.com>
2024-12-01 18:37:21 -0600

    Add work configuration

--- flake.nix
+++ flake.nix
@@ -24,41 +24,59 @@
     self,
     nixpkgs,
     home-manager,
+    nixos-wsl,
     ...
   }: {
-    nixosConfigurations.default = let 
-      usernames = [
-        "jmcelroy"
-        "jmcelroy-dev"
-      ];
-      specialArgs = {inherit usernames;};
-    in nixpkgs.lib.nixosSystem {
-      inherit specialArgs;
-      modules = [
-        ./hosts/default/configuration.nix
-        ./users/jmcelroy/nixos.nix
-        ./users/jmcelroy-dev/nixos.nix
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
+    nixosConfigurations = {
+      default = let 
+        usernames = [
+          "jmcelroy"
+          "jmcelroy-dev"
+        ];
+        specialArgs = {inherit usernames;};
+      in nixpkgs.lib.nixosSystem {
+        inherit specialArgs;
+        modules = [
+          ./hosts/default/configuration.nix
+          ./users/jmcelroy/nixos.nix
+          ./users/jmcelroy-dev/nixos.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
 
-          home-manager.extraSpecialArgs = inputs // specialArgs;
-          home-manager.users.jmcelroy = import ./users/jmcelroy/home.nix;
-          home-manager.users.jmcelroy-dev = import ./users/jmcelroy-dev/home.nix;
-        }
-      ];
-      # modules = [
-      #   ./hosts/default/configuration.nix
-      # ] ++ builtins.concatMap (username: [
-      #   ./users/${username}/nixos.nix
-      #   home-manager.nixosModules.home-manager {
-      #     home-manager.useGlobalPkgs = true;
-      #     home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = inputs // specialArgs;
+            home-manager.users.jmcelroy = import ./users/jmcelroy/home.nix;
+            home-manager.users.jmcelroy-dev = import ./users/jmcelroy-dev/home.nix;
+          }
+        ];
+        # modules = [
+        #   ./hosts/default/configuration.nix
+        # ] ++ builtins.concatMap (username: [
+        #   ./users/${username}/nixos.nix
+        #   home-manager.nixosModules.home-manager {
+        #     home-manager.useGlobalPkgs = true;
+        #     home-manager.useUserPackages = true;
 
-      #     home-manager.extraSpecialArgs = inputs // specialArgs // {inherit username; };
-      #     home-manager.users.${username} = import ./users/${username}/home.nix;
-      #   }
-      # ]) usernames;
+        #     home-manager.extraSpecialArgs = inputs // specialArgs // {inherit username; };
+        #     home-manager.users.${username} = import ./users/${username}/home.nix;
+        #   }
+        # ]) usernames;
+      };
+      work = let
+        usernames = [
+          "jmcelroy-dev"
+        ];
+        specialArgs = {inherit usernames;};
+      in nixpkgs.lib.nixosSystem {
+        inherit specialArgs;
+        modules = [
+          nixos-wsl.nixosModules.default
+          {
+            system.stateVersion = "24.05";
+            wsl.enable = true;
+          }
+        ];
+      };
     };
   };
 }
