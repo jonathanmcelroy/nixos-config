a45885a163d6e55cdef956e6517f81b1039da979
Jonathan McElroy <jonathanpmcelroy@gmail.com>
2025-01-12 13:28:52 -0600

    Use static routes for desktop pc

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -19,15 +19,18 @@
   boot.loader.grub.device = "/dev/nvme0n1";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "jmcelroy-home"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
+  networking = {
+    hostName = "jmcelroy-home"; 
+    networkmanager.enable = true;
+    defaultGateway = "192.168.0.1";
+    nameservers = [ "192.168.0.1" ];
+    interfaces.enp30s0 = {
+      ipv4.addresses = [{
+        address = "192.168.0.100";
+        prefixLength = 24;
+      }];
+    };
+  };
 
   hardware.graphics = {
     enable = true;
