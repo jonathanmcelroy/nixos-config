c0366c39546ef91a1ea018f4e21829cfcb29aed8
Jonathan McElroy <jonathanpmcelroy@gmail.com>
2025-03-02 16:07:19 -0600

    Remove remaining references to jmcelroy-home

--- tests/home-network-test.nix
+++ tests/home-network-test.nix
@@ -6,16 +6,16 @@ in pkgs.testers.runNixOSTest ({
 
   testScript = { nodes, ... }:
   let
-    jmcelroy_home_ip = (pkgs.lib.head nodes.earth.networking.interfaces.eth1.ipv4.addresses).address;
+    earth_ip = (pkgs.lib.head nodes.earth.networking.interfaces.eth1.ipv4.addresses).address;
     server1_ip = (pkgs.lib.head nodes.server1.networking.interfaces.eth1.ipv4.addresses).address;
   in ''
-    jmcelroy_home.start()
+    earth.start()
     server1.start()
 
-    jmcelroy_home.wait_for_unit("default.target")
+    earth.wait_for_unit("default.target")
     server1.wait_for_unit("default.target")
 
-    jmcelroy_home.succeed("ping -c 1 ${server1_ip}")
-    server1.succeed("ping -c 1 ${jmcelroy_home_ip}")
+    earth.succeed("ping -c 1 ${server1_ip}")
+    server1.succeed("ping -c 1 ${earth_ip}")
   '';
 } // import ./base-test.nix inputs catalog)
